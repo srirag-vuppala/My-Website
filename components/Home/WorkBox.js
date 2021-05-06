7d76e6f62057cf024b96bfc96340c279b32d5f47
srirag-vuppala <vuppala@calpoly.edu>
2021-05-05 23:58:40 -0700

    add icons

--- components/Home/WorkBox.js
+++ components/Home/WorkBox.js
@@ -1,9 +1,30 @@
-import React from 'react'
-import { Box, Heading, Text, useColorMode } from "@chakra-ui/react";
+import React from "react";
+import {
+  Center,
+  Box,
+  Heading,
+  Text,
+  Divider,
+  HStack,
+  useColorMode,
+  Tag,
+  Image,
+  Link,
+} from "@chakra-ui/react";
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 
-const WorkBox = ({company, position, startDate, endDate, description, }) => {
-   const { colorMode, toggleColorMode } = useColorMode();
-   function ColorChoose() {
+const WorkBox = ({
+  company,
+  position,
+  companyLogo,
+  companyURL,
+  startDate,
+  endDate,
+  description,
+  techStack,
+}) => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  function ColorChoose() {
     if (colorMode == "light") {
       return "#0a304a";
     } else {
@@ -13,19 +34,29 @@ const WorkBox = ({company, position, startDate, endDate, description, }) => {
   return (
     <>
       <Box
-        my= {4}
+        my={4}
         borderRadius="12px"
         px={[3, null, 6]}
         py={[3, null, 6]}
         borderColor={ColorChoose}
         boxShadow="lg"
       >
-          <Heading>{company}</Heading>
-          <Text>{position}</Text>
-          <Text>{description}</Text>
+        <Link href={companyURL} isExternal>
+          <HStack>
+            <Image boxSize="50px" src={companyLogo} borderRadius="full" />
+            <Heading fontFamily="body">{company}</Heading>
+            <ExternalLinkIcon mx="2px" />
+          </HStack>
+        </Link>
+        &nbsp;
+        <Text>{position}</Text>
+        <Text>{description}</Text>
+        {techStack.map((tech, i) => {
+          return <Tag>{tech}</Tag>;
+        })}
       </Box>
     </>
   );
-}
+};
 
-export default WorkBox
+export default WorkBox;
--- components/Home/WorkExperience.js
+++ components/Home/WorkExperience.js
@@ -1,20 +1,37 @@
-import React from 'react'
-import { Box, Text, Heading, Divider } from '@chakra-ui/react'
-import BorderBox from '@/components/BorderBox'
-import WorkBox from '@/components/Home/WorkBox'
+import React from "react";
+import { Box, Text, Heading, Divider } from "@chakra-ui/react";
+import BorderBox from "@/components/BorderBox";
+import WorkBox from "@/components/Home/WorkBox";
 
 const WorkExperience = () => {
-    return (
-        <>
-        <Box p={6}>
-            <BorderBox>
-               <Heading fontFamily="head">Work Experience</Heading>
-               <Divider/>
-               <WorkBox company="TRIO Achievers" position="Student Assistant" startDate="January 2021" endDate="March 2021" description="sjfdljsldf" />
-            </BorderBox>
-        </Box>
-        </>
-    )
-}
+  return (
+    <>
+      <Box p={6}>
+        <BorderBox>
+          <Heading fontFamily="head">Work Experience</Heading>
+          <Divider />
+          <WorkBox
+            company="TRIO Achievers"
+            position="Student Assistant"
+            startDate="January 2021"
+            endDate="March 2021"
+            description="sjfdljsldf"
+            techStack={["HTML", "CSS"]}
+          />
+          <WorkBox
+            company="IATPP"
+            position="Student Assistant"
+            companyLogo="https://res.cloudinary.com/dtbzgpkps/image/upload/v1620282769/IATPP_262ad391d3.jpg"
+            companyURL="https://iatpp.calpoly.edu/"
+            startDate="April 2021"
+            endDate="Present"
+            description="Worked on the AI4Reporters team helping them set up their website to render the articles"
+            techStack={["HTML", "CSS", "JavaScript", "React", "Flask"]}
+          />
+        </BorderBox>
+      </Box>
+    </>
+  );
+};
 
-export default WorkExperience
+export default WorkExperience;
